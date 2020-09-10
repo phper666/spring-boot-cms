d560d3fb48b0b73de9157b567534281155361a86
liyuzhao <8603583yuPENG>
2020-09-10 15:58:44 +0800

    测试

---
+++ demo/src/main/java/com/github/phper666/App.java
@@ -0,0 +1,14 @@
+package com.github.phper666;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyuzhao
+ */
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ study-common/src/main/resources/db/migration/V1.0.0__test-table.sql
@@ -0,0 +1,17 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
+-- 插入数据
+INSERT INTO `test`.`user` (`username`, `password`) VALUES ('test', '22342');
